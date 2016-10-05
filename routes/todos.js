e3fb8b66b4e91ed548857d4939bb2d9eb9bb8278
Your Name <you@example.co>
2016-10-05 21:36:40 +0200

    Connection do MongoDB using js

--- routes/todos.js
+++ routes/todos.js
@@ -7,9 +7,35 @@
 
 var express = require('express');
 var router  = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:admin@ds049476.mlab.com:49476/meantodos_slawek0102',['todos'])
+
+
+//Get ToDos
 
 router.get('/todos', function (req, res, next) {
-  res.send('TODOS API')
+  db.todos.find(function (err, todos) {
+    if (err){
+      res.send(err);
+    } else {
+      res.json(todos);
+    }
+  })
 });
+
+//Get Todo
+
+router.get('/todo/:id', function (req, res, next) {
+  db.todos.findOne({
+
+  },function (err, todos) {
+    if (err){
+      res.send(err);
+    } else {
+      res.json(todos);
+    }
+  })
+});
+
 
 module.exports = router;
